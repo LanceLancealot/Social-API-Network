bafd85723afebfc0d9901c5995e1fb85f97a9c47
Maxfield Kraus <maxfield.kraus@gmail.com>
2023-12-10 16:16:19 -0500

    create repo

---
+++ controllers/user-controller.js
@@ -0,0 +1,121 @@
+const { User } = require('../models');
+
+const userController = {
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Get a single user by ID
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.userId })
+      .populate({
+        path: 'thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => {
+        // If no user is found, send 404 status
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Create a new user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Update a user by ID
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a user by ID
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.userId })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        // Bonus: Remove the user's associated thoughts
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+      })
+      .then(() => {
+        res.json({ message: 'User and associated thoughts deleted' });
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Add a new friend to a user's friend list
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Remove a friend from a user's friend list
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
+module.exports = userController;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// Routes
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
